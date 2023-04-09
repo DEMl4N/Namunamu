0eee735e08c16e4fc4e72a8adfdd99b70be58ede
DEMl4N <delfirmamentos@gmail.com>
2023-04-10 01:52:21 +0900

    Feat: Wait until the lecture finished

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.remained_seconds) {
+        console.log(`I got you ${message.remained_seconds}`)
+      setTimeout(() => {
+        // Send a message back to the content script indicating that the sleep is over
+        chrome.tabs.sendMessage(sender.tab.id, 'sleepComplete');
+      }, message.remained_seconds * 1000);
+    }
+});
