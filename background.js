80645184ed081557c888d289538a1dd8fa928edb
DEMl4N <delfirmamentos@gmail.com>
2023-04-10 21:16:53 +0900

    Fix: Message String

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.remained_seconds) {
-        console.log(`I got you ${message.remained_seconds}`)
+      console.log(`I got you ${message.remained_seconds}`)
       setTimeout(() => {
         // Send a message back to the content script indicating that the sleep is over
-        chrome.tabs.sendMessage(sender.tab.id, 'sleepComplete');
+        chrome.tabs.sendMessage(sender.tab.id, 'course_done');
       }, message.remained_seconds * 1000);
     }
 });
