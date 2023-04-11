66adc34a204ef9f27a119e91a9c4bdc7cc8ef38e
DEMl4N <delfirmamentos@gmail.com>
2023-04-12 00:54:26 +0900

    Feat: Changing course automatically / setTimeout to online_view

--- content.js
+++ content.js
@@ -1,15 +1,10 @@
 // Made by 김뀨뀨
-//import { toDoList } from "./list.js"
-
-const toDoList =
-[
-
-]
+import { toDoList } from "./list.js"
 
 const cuk_URI = "https://e-cyber.catholic.ac.kr/ilos/main/main_form.acl"
 const course_URI = "https://e-cyber.catholic.ac.kr/ilos/st/course/submain_form.acl"
 const online_lecture_URI = "https://e-cyber.catholic.ac.kr/ilos/st/course/online_list_form.acl"
-const online__view_URI = "https://e-cyber.catholic.ac.kr/ilos/st/course/online_view_form.acl"
+const online_view_URI = "https://e-cyber.catholic.ac.kr/ilos/st/course/online_view_form.acl"
 
 let streaming_lecture = null
 let streaming_time = 0
@@ -45,18 +40,22 @@ function time_to_seconds(time) {
 function cuk_main() {
     console.log(`${cuk_URI} connected`);
 
-    var lectures = Array.from(document.getElementsByClassName("sub_open"))
+    let lecture_id_list = []
+    let lectures = Array.from(document.getElementsByClassName("sub_open"))
 
-    if (toDoList.length == 0) {
+    if (toDoList.length != 0) {
         lectures.filter(element => {
             for (const lectureName of toDoList){
                 if (element.innerHTML.includes(lectureName)){
+                    lecture_id_list.push(element.innerText)
                     return true
                 }
             }
             return false
         })
     }
+
+    sessionStorage.setItem("lecture_id_list", JSON.stringify(lecture_id_list))
 }
 
 function course() {
@@ -82,22 +81,74 @@ async function online_lecture() {
             await delay(300)
             let lecture_form = document.getElementById("lecture_form")
 
-            const is_good_to_go = 
-            !lecture_form.innerText.includes("학습 기간이 아닙니다.") 
-            && 
-            lecture_form.getElementsByClassName('loader').length == 0;
-    
-            if (is_good_to_go) {
-                let lecture_list = lecture_form.getElementsByClassName("lecture-box")
-                console.log(lecture_list)
-                parse_lectures(lecture_list)
+            const is_early = lecture_form.innerText.includes("학습 기간이 아닙니다.") 
+            const is_loading = lecture_form.getElementsByClassName('loader').length > 0;
+
+            if (is_loading) {
+                continue
+            }
+
+            if (is_early) {
                 break
             }
+    
+            let lecture_list = lecture_form.getElementsByClassName("lecture-box")
+            console.log(lecture_list)
+            parse_lectures(lecture_list)
+            break
+
         }
 
         if (streaming_lecture !== null && streaming_time != 0) {
             break
         }
+    }
+
+    // no more lecture in this course
+    if (streaming_lecture === null) {
+        // open subject
+        document.querySelector("#subject-span").click()
+        delay(300)
+        let subject_room = document.querySelector("#subject_room")
+        const title = document.querySelector(".welcome_subject").innerText
+        const lecture_id = title.match(/\((.*?)\)/)[0]
+        
+        const lecture_id_list = JSON.parse(sessionStorage.getItem("lecture_id_list"))
+
+        if (lecture_id_list.length > 0) {
+            const new_lecture_id_list = lecture_id_list.filter(element => !element.includes(lecture_id))
+            sessionStorage.setItem("lecture_id_list", JSON.stringify(new_lecture_id_list))
+            const next_lecture_id = new_lecture_id_list[0]
+            const lecture_rooms = document.querySelector(".roomGo")
+
+            for (room of lecture_rooms) {
+                if (room.innerText.includes(next_lecture_id)) {
+                    room.click()
+                }
+            }
+
+            console.log("강의가 왜 없지")
+        }
+        else {
+            alert("전 강의 수강 완료")
+            window.location.href = cuk_URI
+        }
+    }
+}
+
+function online_view() {
+    let running_time = JSON.parse(sessionStorage.getItem("streaming_time"))
+    running_time = (running_time !== null)? running_time : 0;
+
+    console.log(running_time)
+    setTimeout(() => {
+        const exit_button = document.getElementById("close_")
+        exit_button.click()
+    }, running_time * 1000)
+
+    // ignore time checking message
+    window.confirm = function(message) {
+        return true
     }
 }
 
@@ -110,11 +161,10 @@ function parse_lectures(lecture_list) {
             const view_button = view_buttons[i]
 
             const dates = lecturebox.innerHTML.matchAll(/\d{12}/g)
-            const start_date = dates.next()
-            const now_date = dates.next()
-            const end_date = dates.next()
+            const end_date = dates.next().value[0]
+            const now_date = dates.next().value[0]
 
-            if (now_date < start_date || now_date > end_date) {
+            if (now_date > end_date) {
                 break
             }
             
@@ -125,7 +175,7 @@ function parse_lectures(lecture_list) {
                 continue
             }
 
-            const remained_seconds = calcutate_remained_seconds(i, lecturebox)
+            const remained_seconds = calcutate_remained_seconds(i, lecturebox) 
             streaming_lecture = view_button
             streaming_time = remained_seconds
 
@@ -170,53 +220,45 @@ function calcutate_remained_seconds(idx, lecturebox) {
 }
 
 function start_streaming() {
-    chrome.runtime.sendMessage({remained_second: streaming_time}, function(response) {
-        console.log(response.message);
-    });
-
+    console.log("*(^u^)*");
+    sessionStorage.setItem("streaming_time", JSON.stringify(streaming_time))
     streaming_lecture.click()
 }
 
 if (window.location.href == cuk_URI) {
     if (confirm("나무나무를 시작하시겠습니까?")){
-        localStorage.setItem('isExtensionOn', JSON.stringify(true))
+        sessionStorage.setItem('isExtensionOn', JSON.stringify(true))
     }
     else {
-        localStorage.setItem('isExtensionOn', JSON.stringify(false))
+        sessionStorage.setItem('isExtensionOn', JSON.stringify(false))
     }
 
-    if (JSON.parse(localStorage.getItem('isExtensionOn')) == true){
+    if (JSON.parse(sessionStorage.getItem('isExtensionOn')) == true){
         cuk_main()
         easterEgg()
     }
 }
 
 if (window.location.href == course_URI) {
-    if (JSON.parse(localStorage.getItem('isExtensionOn')) == true) {
+    if (JSON.parse(sessionStorage.getItem('isExtensionOn')) == true) {
         course()
     }
 }
 
 if (window.location.href.includes(online_lecture_URI)) {
-    if (JSON.parse(localStorage.getItem('isExtensionOn')) == true) {
+    if (JSON.parse(sessionStorage.getItem('isExtensionOn')) == true) {
         online_lecture()
     }
 }
 
-if (window.location.href.includes(online__view_URI)) {
-    if (JSON.parse(localStorage.getItem('isExtensionOn')) == true) {
-        chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-            if (message === 'course_done') {
-                console.log("Done")
-                const exit_button = document.getElementById("close_")
-                exit_button.click()
-            }
-        })
+if (window.location.href.includes(online_view_URI)) {
+    if (JSON.parse(sessionStorage.getItem('isExtensionOn')) == true) {
+        online_view()
     }
 }
 
 
-
+////////////////////////////// *(^u^)* \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 function easterEgg() {
     const user_name = document.getElementById("user").innerHTML
     document.getElementById("user").innerHTML = "나무 심는 " + user_name
